99b38d395a6312a946ce2b17e3ea6b0e236255d9
Alexander Öberg <stativetroller7@gmail.com>
2023-12-22 15:56:37 +0100

    Add project files.

---
+++ LexiconLMS/Client/Program.cs
@@ -0,0 +1,25 @@
+using LexiconLMS.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace LexiconLMS.Client;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddHttpClient("LexiconLMS.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+        // Supply HttpClient instances that include access tokens when making requests to the server project
+        builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("LexiconLMS.ServerAPI"));
+
+        builder.Services.AddApiAuthorization();
+
+        await builder.Build().RunAsync();
+    }
+}
