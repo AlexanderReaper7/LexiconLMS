1befebae0fc4a246971b62f9ce1151f7695e82ae
Alexander Öberg <stativetroller7@gmail.com>
2023-12-28 10:26:13 +0100

    add api controllers

---
+++ LexiconLMS/Server/Controllers/ActivityTypesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LexiconLMS.Server.Data;
+using LexiconLMS.Shared.Entities;
+
+namespace LexiconLMS.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityTypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActivityTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ActivityTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ActivityType>>> GetActivityTypes()
+        {
+          if (_context.ActivityTypes == null)
+          {
+              return NotFound();
+          }
+            return await _context.ActivityTypes.ToListAsync();
+        }
+
+        // GET: api/ActivityTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ActivityType>> GetActivityType(string id)
+        {
+          if (_context.ActivityTypes == null)
+          {
+              return NotFound();
+          }
+            var activityType = await _context.ActivityTypes.FindAsync(id);
+
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            return activityType;
+        }
+
+        // PUT: api/ActivityTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutActivityType(string id, ActivityType activityType)
+        {
+            if (id != activityType.Name)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(activityType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActivityTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ActivityTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ActivityType>> PostActivityType(ActivityType activityType)
+        {
+          if (_context.ActivityTypes == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.ActivityTypes'  is null.");
+          }
+            _context.ActivityTypes.Add(activityType);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ActivityTypeExists(activityType.Name))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetActivityType", new { id = activityType.Name }, activityType);
+        }
+
+        // DELETE: api/ActivityTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivityType(string id)
+        {
+            if (_context.ActivityTypes == null)
+            {
+                return NotFound();
+            }
+            var activityType = await _context.ActivityTypes.FindAsync(id);
+            if (activityType == null)
+            {
+                return NotFound();
+            }
+
+            _context.ActivityTypes.Remove(activityType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ActivityTypeExists(string id)
+        {
+            return (_context.ActivityTypes?.Any(e => e.Name == id)).GetValueOrDefault();
+        }
+    }
+}
