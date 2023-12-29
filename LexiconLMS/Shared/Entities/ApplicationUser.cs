82e6a4a81966ea6821673c867af0551cf2a127dd
Alexander Öberg <stativetroller7@gmail.com>
2023-12-29 11:50:40 +0100

    fix seed data accounts
    also add first name and last name to user

--- LexiconLMS/Shared/Entities/ApplicationUser.cs
+++ LexiconLMS/Shared/Entities/ApplicationUser.cs
@@ -7,5 +7,6 @@ namespace LexiconLMS.Shared.Entities;
 public class ApplicationUser : IdentityUser
 {
     public Course? Course { get; set; }
-
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
 }
