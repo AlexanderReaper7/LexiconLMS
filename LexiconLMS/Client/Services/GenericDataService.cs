e377fc0af5652b3b73b2be277d03a8f11a5cea50
Elias Rafo <rafoelias@hotmail.com>
2023-12-31 14:25:33 +0100

    Fix GenericDataService

--- LexiconLMS/Client/Services/GenericDataService.cs
+++ LexiconLMS/Client/Services/GenericDataService.cs
@@ -3,10 +3,7 @@ using System.IO;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
-<<<<<<< HEAD
-=======
 using static System.Net.WebRequestMethods;
->>>>>>> 8187049051dd903942128043174eb47d4917651f
 
 namespace LexiconLMS.Client.Services;
 
@@ -14,11 +11,7 @@ public interface IGenericDataService
 {
     protected const string json = "application/json";
     Task<T?> GetAsync<T>(string path, string contentType = json);
-<<<<<<< HEAD
     Task<bool> AddAsync<T>(string path, T objectToAdd);
-=======
-	Task<bool> AddAsync<T>(string path, T objectToAdd);
->>>>>>> 8187049051dd903942128043174eb47d4917651f
 	Task<bool> UpdateAsync<T>(string path, T objectToUpdate);
 	Task<bool> DeleteAsync(string path);
 }
@@ -47,12 +40,7 @@ public class GenericDataService : IGenericDataService
 		var result = JsonSerializer.Deserialize<T>(stream, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 		return result;
 	}
-
-	public async Task<bool> AddAsync<T>(string path, T objectToAdd)
-	{
-
-<<<<<<< HEAD
-    }
+	
 
     public async Task<bool> AddAsync<T>(string path, T objectToAdd)
     {
@@ -104,53 +92,4 @@ public class GenericDataService : IGenericDataService
         return false;
 
     }
-=======
-		string json = JsonSerializer.Serialize(objectToAdd);
-
-		var request = new HttpRequestMessage(HttpMethod.Post, path);
-		var httpContent = new StringContent(json, new MediaTypeHeaderValue(Application.Json));
-		request.Content = httpContent;
-
-		var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
-		if (response.IsSuccessStatusCode)
-		{
-			return true;
-		}
-		return false;
-	}
-
-	public async Task<bool> UpdateAsync<T>(string path, T objectToUpdate)
-	{
-
-		string json = JsonSerializer.Serialize(objectToUpdate);
-
-		var request = new HttpRequestMessage(HttpMethod.Put, path);
-		var httpContent = new StringContent(json, new MediaTypeHeaderValue(Application.Json));
-		request.Content = httpContent;
-
-		var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
-		if (response.IsSuccessStatusCode)
-		{
-			return true;
-		}
-		return false;
-	}
-
-	public async Task<bool> DeleteAsync (string path)
-	{
-
-		var request = new HttpRequestMessage(HttpMethod.Delete, path);
-
-		var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
-		if (response.IsSuccessStatusCode)
-		{
-			return true;
-		}
-		return false;
-
-	}
->>>>>>> 8187049051dd903942128043174eb47d4917651f
 }
